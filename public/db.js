1f8a9e5b38a6544bb45b3b5b7d15c475acd0d3e0
Rick Levy <ricklevy21@gmail.com>
2020-08-04 21:07:53 -0600

    added the saveRecord function that adds a record to the indexedDB when invoked

--- public/db.js
+++ public/db.js
@@ -1,14 +1,44 @@
+//define a variable called request. This request creates an indexedDB on the browser (window) and opens it. in this case it is called budget.
 const request = window.indexedDB.open("budget", 1);
 
+//should htere be a change to the schema (added an object store, changed the object store), the version, which is referred to after the name of the db, should be incremented.
+//in the case of a new version, the code below is run. here is creates/adds the object store.
 request.onupgradeneeded = function(event) {
     // create object store
     //object store is analagous to a table/colleciton in a db.
     //in this app, I am using it to store all pending transactions that are part of the budget "db"
+    //set the db variable to the result, so I can add the object store to that db
    const db = event.target.result;
    db.createObjectStore("pendingTransactions", { autoIncrement: true });
- };
+ }
 
-request.onsuccess = event => {
+ request.onsuccess = event => {
+    //when or if the request to make the indexedDB is successfull, execute this code
     console.log("success!")
     console.log(request.result)
-};
+}
+
+request.onerror = event => {
+
+}
+
+function saveRecord(record) {
+    //we have lost connection
+    //create a transaction record on the indexedDB, with readwrite access
+    //go the the database, create a transaction. transaction method takes in 2 parameters: the object stor in the form of an array (in case there are multiple) and what you want to do with it, in this case give it read and write access, to read/write from/to the db
+    const transaction = db.transaction(["pendingTransaction"], "readwrite")
+    
+    //access the pendingTransactioin object store
+    const store = transaction.objectStore("pendingTransactions")
+
+    //add record to the object store with the add method
+    store.add(record)
+}
+
+function checkDatabase() {
+
+}
+
+//listen for the applicaiton coming back online
+//when the app is back online, run the function checkDatabase
+window.addEventListener("online", checkDatabase)
